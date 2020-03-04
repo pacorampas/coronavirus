1ed5ee6d37c62177c6ff2a59f8189831db9fa516
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2020-03-04 13:21:27 +0100

    feat: add analytics

---
+++ src/js/analytics.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyASj5WIF1R0S11v8mBTKx-m5f2uKHKhyVs",
+  authDomain: "infectit-coronavirus.firebaseapp.com",
+  databaseURL: "https://infectit-coronavirus.firebaseio.com",
+  projectId: "infectit-coronavirus",
+  storageBucket: "infectit-coronavirus.appspot.com",
+  messagingSenderId: "458480879234",
+  appId: "1:458480879234:web:9f7c6974b8d0928707d69b",
+  measurementId: "G-6Q4DXN7D89"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
